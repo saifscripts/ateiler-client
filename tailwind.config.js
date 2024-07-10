6cd0782d895cb886d102d02eea0ae09d4d90f57d
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-07-10 20:42:34 +0600

    setup tailwind keep-react redux redux-toolkit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+import keepPreset from 'keep-react/preset';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/keep-react/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {
+      colors: {},
+    },
+  },
+  plugins: [],
+  presets: [keepPreset],
+};
