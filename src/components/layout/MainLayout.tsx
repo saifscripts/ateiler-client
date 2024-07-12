6ea0aa61c83cdaac0022880e6cc5d94309063cb1
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-07-12 09:36:05 +0600

    feat: footer

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom';
+import { Footer } from './footer';
 import { Navbar } from './navbar';
 
 const MainLayout = () => {
@@ -6,6 +7,7 @@ const MainLayout = () => {
     <div className="bg-primary-25">
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
--- src/features/featured-products/FeaturedProducts.tsx
+++ src/features/featured-products/FeaturedProducts.tsx
@@ -3,7 +3,7 @@ import Title from './Title';
 
 const FeaturedProducts = () => {
   return (
-    <section className="container">
+    <section id="featured-products" className="container">
       <div className="py-6 bg-white rounded-lg">
         <Title />
         <Products />
