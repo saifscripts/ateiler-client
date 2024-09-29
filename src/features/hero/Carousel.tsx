936425b496b751b537d6e5ebbc4c1fe8fa67622b
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-09-29 08:04:28 +0600

    finish category section

--- src/features/hero/Carousel.tsx
+++ src/features/hero/Carousel.tsx
@@ -9,10 +9,14 @@ import {
 
 const CarouselComponent = () => {
   return (
-    <Carousel options={{ loop: true }} plugins={[autoPlay()]}>
+    <Carousel
+      options={{ loop: true }}
+      plugins={[autoPlay()]}
+      className="order-1 xl:order-2"
+    >
       <CarouselSlides>
         {[1, 2, 3, 4, 5].map((slide) => (
-          <CarouselItem key={slide}>
+          <CarouselItem key={slide} className="pl-0">
             <div className="flex h-96 items-center justify-center rounded-xl border border-metal-100 bg-primary-200 dark:border-metal-900 dark:bg-metal-900">
               <CarouselPrevButton />
               <h1 className="text-heading-1 font-medium text-metal-900 dark:text-white">
--- src/features/hero/Hero.tsx
+++ src/features/hero/Hero.tsx
@@ -1,9 +1,9 @@
-import { CategorySection } from '../category-section';
 import Carousel from './Carousel';
+import CategorySection from './CategorySection';
 
 const Hero = () => {
   return (
-    <div className="container w-full grid grid-cols-[1fr_4fr] py-6 gap-6">
+    <div className="container w-full grid grid-cols-1 xl:grid-cols-[1fr_4fr] py-6 gap-6">
       <CategorySection />
       <Carousel />
     </div>
